a714cddfbce1a4aca2eddbf1f9a3e66c288b6fb7
irina_g <igv2igv@gmail.com>
2024-11-18 18:08:57 +0200

    Added generateQuestion() function to randomly select correct and incorrect flags

--- js/geography.js
+++ js/geography.js
@@ -1,7 +1,16 @@
 const API_URL = 'https://restcountries.com/v3.1/all';
 
+let correctAnswer = null;
+
 async function fetchCountries() {
   const response = await fetch(API_URL);
   const data = await response.json();
   return data;
+}
+
+function generateQuestion(countries) {
+  const correctCountry = countries[Math.floor(Math.random() * countries.length)];
+  correctAnswer = correctCountry.flags.png; // URL правильного флага
+  const options = [correctCountry]; // Добавить правильный ответ
+  return options;
 }
